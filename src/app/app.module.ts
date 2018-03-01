42fb56b12e04b1f261cf914d6e2b25804031a809
Pavan Narumalla <pavan.narumalla@aig.com>
2018-03-01 14:26:12 +0530

    Enhancement : upto Two Way Binding using ngModel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
 import { CoursesComponent } from './courses.component';
 import { CourseComponent } from './course/course.component';
+import { CoursesService } from './courses.service';
 
 
 
@@ -15,9 +17,12 @@ import { CourseComponent } from './course/course.component';
     CourseComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [
+    CoursesService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -1,19 +1,52 @@
 import {Component} from '@angular/core';
+import { CoursesService } from './courses.service';
 
 
 @Component({
-    selector : '{{title}}',
+    selector : 'courses',
     template : 
         `
+        <h2>{{ title }}</h2>
         <ul>
             <li *ngFor = "let course of courses">
                 {{course}}
             </li>
         </ul>
+        <input [(ngModel)] = "email" (keyUp.Enter) = "onKeyUp()"/>
+        
+        <div (click) = "onDivClick($event)">
+            <button class = "btn btn-primary" 
+            [class.isActive] = "isActive" 
+            [style.backgroundColor] = "isActive ? 'black': 'Red'"
+            (click) = "onSave($event)"
+            >Save</button>
+        </div>
+        
         `,
 })
 export class CoursesComponent {
     title = "List Of Courses";
-    courses = ["Course 1", "Course 2", "Course 3"];
+    courses;
+    isActive = true;
+    email = "example@gmail.com";
+    constructor(service : CoursesService){
+        this.courses = service.getCourses();
+    }
+
+    onSave($event){
+        $event.stopPropagation();
+        console.log("Save Button Clicked !");
+        console.log($event);
+    }
+
+    onDivClick($event){
+        console.log("Clicked on Div ! ");
+        console.log($event);
+    }
+
+    onKeyUp(email){
+        console.log("Enter was pressed !!");
+        console.log("input : ", this.email);
+    }
 
 }
